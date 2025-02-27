a2219548dedee4c294d6a7f621932bb48fd3756b
Ammaranjum <ammaranjum@users.noreply.github.com>
2025-02-27 13:18:55 +0500

    Resolved merge conflicts

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ console.log(original.details.price); // 2.5 (unchanged)
 
 //using json.parse
 
-const original = {
+const smart = {
   name: "Donut",
   flavors: ["chocolate", "vanilla"],
 };
 
-const copy = JSON.parse(JSON.stringify(original));
+const copy = JSON.parse(JSON.stringify(smart));
 
 copy.flavors.push("strawberry");
-console.log(original.flavors); // ["chocolate", "vanilla"]
+console.log(smart.flavors); // ["chocolate", "vanilla"]
 
 //lodash
 import cloneDeep from "lodash/cloneDeep";
 
-const original = {
+const original2 = {
   date: new Date(),
   map: new Map([["key", "value"]]),
 };
 
-const copy = cloneDeep(original);
+const copy = cloneDeep(original2);
 
-console.log(copy.date === original.date); // false (new object)
+console.log(copy.date === original2.date); // false (new object)
